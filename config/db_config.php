80cbbabbe287bd5bb0260a388ffec81e3b0236f8
CaliKane <accardo.steven@gmail.com>
2015-11-22 00:43:04 -0800

    Fith Commit: Functional guestbook that stores user input into server

---
+++ config/db_config.php
@@ -0,0 +1,9 @@
+<!--PHP config file that stores all of the data needed to connect-->
+<?php
+
+	$db_host = "localhost";
+	$db_user = "accardoo_webmgr";
+	$db_password = "WEbsite12!@";
+	$db_name = "accardoo_website";
+	
+?>
