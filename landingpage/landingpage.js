b0ec1d5f50d91fef9e8168ccba6949a6cb1b9bd7
CaliKane <accardo.steven@gmail.com>
2016-01-03 23:10:56 -0800

    12th Commit: Used jQuery to make buttons expand and contract on hover

---
+++ landingpage/landingpage.js
@@ -0,0 +1,21 @@
+$(function(){
+	
+	$('#title1').hover(function(){
+		$(this).css('font-size', '+=4px');		
+		}, function(){
+		$(this).css('font-size', '-=4px');
+		});
+		
+	$('#title2').hover(function(){
+		$(this).css('font-size', '+=4px');		
+		}, function(){
+		$(this).css('font-size', '-=4px');
+		});	
+
+	$('#title3').hover(function(){
+		$(this).css('font-size', '+=4px');		
+		}, function(){
+		$(this).css('font-size', '-=4px');
+		});
+	
+});
